3096917bb013ea36cf9bdf7787ed98b2450ef9d4
EvaGraceSmith <smithevagrace@gmail.com>
2022-11-24 23:33:25 -0600

    added more js

--- js/app.js
+++ js/app.js
@@ -70,12 +70,12 @@ console.log ('Hello from the js file');
 
 // global variable
 Ducks.allDucksArray = [];
-let duckContainer = document.querySelector('section');
+let ducksContainer = document.querySelector('section');
 let resultButton = document.querySelector('section + div');
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
-console.log(pizzaContainer, resultButton, image1, image2, image3,);
+console.log(ducksContainer, resultButton, image1, image2, image3,);
 
 let clicks = 0;
 let maxClicks = 10;
@@ -96,25 +96,112 @@ function Ducks(name, src){
 
 }
 
-console.log('do we have Ducks? ' Ducks.allDucksArray);
+console.log('do we have Ducks? ' , Ducks.allDucksArray);
 
 // function helper for randomization
 function getRandomNumber(){
     return Math.floor(Math.random() * Ducks.allDucksArray.length);
 }
 
-//function to render our pizzas
+//function to render our ducks
 function renderDucks(){
     let duck1 = getRandomNumber();
     let duck2 = getRandomNumber();
     let duck3 = getRandomNumber();
     // console.log(duck1,duck2,duck3);
   
-    while(duck1 === duck2){
+    while(duck1 === duck3){
+        duck3 = getRandomNumber();
+    }
+
+    while(duck1 === duck2 || duck3 === duck2){
       duck2 = getRandomNumber();
     }
-    // console.log(duck1,duck2);
 
 
-    
+     console.log(duck1,duck2);
+
+      //capture some data about images so that we can track the data in our objects
+  //update the src for the new image to be seen after each click
+  //                     assets/images/brickOvenPizza.jpg
+  image1.src = Ducks.allDucksArray[duck1].src;
+  image2.src = Ducks.allDucksArray[duck2].src;
+  image3.src = Ducks.allDucksArray[duck3].src;
+  //update alt attribute
+  image1.alt = Ducks.allDucksArray[duck1].name;
+  image2.alt = Ducks.allDucksArray[duck2].name;
+  image2.alt = Ducks.allDucksArray[duck3].name;
+  //times shown
+  Ducks.allDucksArray[duck1].views++;
+  Ducks.allDucksArray[duck2].views++;
+  Ducks.allDucksArray[duck3].views++;
+
+}//closes our renderDucks function
+
+//function to handle our clicks
+function handleDucksClick(event){
+    console.log('we made it to the click: ', event);
+    if(event.target === ducksContainer){
+      alert('please click on a duck');
+    }
+    //how many time they vote total clicks
+    clicks++;
+    let clickDucks = event.target.alt;
+    console.log(clickDucks);
+    for(let i = 0; i < Ducks.allDucksArray.length; i++){
+      if(clickDucks === Ducks.allDucksArray[i].name){
+        //this.click from duck object
+        // update the ducks individual clicks
+        Ducks.allDucksArray[i].click++;
+        break;
+      }
+    }
+
+      //do we have max attempts completed 10 votes
+  if(clicks === maxClicks){
+ducksContainer.removeEventListener('click', handleDucksClick);
+    //enable the button to see the results
+    resultButton.addEventListener('click', renderResults);
+    ducksContainer.className = 'no-voting';
+  } else {
+    renderDucks();
+  }
 }
+
+//function for our render of results to a list
+function renderResults(){
+    let ul = document.querySelector('ul');
+    for(let i = 0; i < Ducks.allDucksArray.length; i++){
+      let li = document.createElement('li');
+      li.textContent = `${Ducks.allDucksArray[i].name} had ${Ducks.allDucksArray[i].views} views and were clicked on ${Ducks.allDucksArray[i].click} times`;
+      ul.appendChild(li);
+    }
+  
+  }
+
+  //duck objects and call the constructor with the new operator
+
+new Ducks('bag', '../img/bag.jpg');
+new Ducks('banana', '../img/banana.jpg');
+new Ducks('bathroom', '../img/bathroom.jpg');
+new Ducks('boots', '../img/boots.jpg');
+new Ducks('breakfast', '../img/breakfast.jpg');
+new Ducks('bubblegum', '../img/bubblegum.jpg');
+new Ducks('chair', '../img/chair.jpg');
+new Ducks('cthulhu', '../img/cthulhu.jpg');
+
+new Ducks('dog-duck', '../img/dog-duck.jpg');
+new Ducks('dragon', '../img/dragon.jpg');
+new Ducks('pen', '../img/pen.jpg');
+new Ducks('pet-sweep', '../img/pet-sweep.jpg');
+new Ducks('scissors', '../img/scissors.jpg');
+new Ducks('shark', '../img/shark.jpg');
+new Ducks('sweep', '../img/sweep.jpg');
+new Ducks('tauntaun', '../img/tauntaun.jpg');
+new Ducks('unicorn', '../img/unicorn.jpg');
+
+//call all functions
+renderDucks();
+
+//add our event listener to run our handleClick()
+ducksContainer.addEventListener('click', handleDucksClick);
