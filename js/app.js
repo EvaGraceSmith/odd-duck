1976bfbcf9d0ab6549f655a28658271c5c415773
EvaGraceSmith <smithevagrace@gmail.com>
2022-11-26 11:24:36 -0600

    changed color styling

--- js/app.js
+++ js/app.js
@@ -2,72 +2,6 @@
 console.log ('Hello from the js file');
 
 
-// 1) As a user, I would like to display three unique products by chance 
-// so that the viewers can pick a favorite.
-
-// Create a constructor function that creates an object 
-// associated with each product, and has the following properties:
-// Name of the product
-// File path of image
-// Times the image has been shown
-
-// Create an algorithm that will randomly generate three unique 
-// product images from the images directory and display them 
-// side-by-side-by-side in the browser window.
-
-// For each of the three images, increment its property of times 
-// it has been shown by one.
-
-// Attach an event listener to the section of the HTML page 
-// where the images are going to be displayed.
-
-// Once the users ‘clicks’ a product, generate three new products 
-// for the user to pick from.
-
-
-
-// 2.) As a user, I would like to track the selections made by viewers 
-// so that I can determine which products to begin production on.
-
-// In the constructor function define a property to hold the number 
-// of times a product has been clicked.
-
-// After every selection by the viewer, update the newly added
-//  property to reflect if it was clicked.
-
-// 3.)As a user, I would like to control the number of rounds a user
-//  is presented with so that I can control the voting session 
-//  duration.
-
-// By default, the user should be presented with 25 rounds of 
-// voting before ending the session.
-
-// Keep the number of rounds in a variable to allow the number 
-// to be easily changed for debugging and testing purposes.
-
-
-
-// 4.) As a user, I would like to view a report of results after 
-// all rounds of voting have concluded so that I can evaluate 
-// which products were the most popular.
-
-// Create a property attached to the constructor function itself 
-// that keeps track of all the products that are currently being 
-// considered.
-
-// After voting rounds have been completed, remove the event 
-// listeners on the product.
-
-// Add a button with the text View Results, which when clicked 
-// displays the list of all the products followed by the votes 
-// received, and number of times seen for each. 
-// Example: banana had 3 votes, and was seen 5 times.
-
-// NOTE: Displayed product names should match the file name for 
-// the product. Example: the product represented with dog-duck.jpg 
-// should be displayed to the user as exactly “dog-duck” when 
-// the results are shown.
-
 // global variable
 Ducks.allDucksArray = [];
 let ducksContainer = document.querySelector('section');
@@ -78,10 +12,10 @@ let image3 = document.querySelector('section img:nth-child(3)');
 console.log(ducksContainer, resultButton, image1, image2, image3,);
 
 let clicks = 0;
-let maxClicks = 10;
+let maxClicks = 25;
 console.log('click tracking', {clicks, maxClicks});
 
-//constructor functions    src refers to the <img src="assets/images/brickOvenPizza.jpg" />
+//constructor functions    src refers to the <img src="../img/bag.jpg" />
 function Ducks(name, src){
     this.name = name;
     this.src = src;
@@ -90,7 +24,7 @@ function Ducks(name, src){
     //times clicked on
     this.click = 0;
 
-      //   As we create new instances of our pizza objects we can push those into array using the 'this' and the .push()
+      //   As we create new instances of our duck objects we can push those into array using the 'this' and the .push()
   // built in array method
   Ducks.allDucksArray.push(this);
 
@@ -122,8 +56,7 @@ function renderDucks(){
      console.log(duck1,duck2);
 
       //capture some data about images so that we can track the data in our objects
-  //update the src for the new image to be seen after each click
-  //                     assets/images/brickOvenPizza.jpg
+  //update the src for the new image to be seen after each click  ../img/bag.jpg                   
   image1.src = Ducks.allDucksArray[duck1].src;
   image2.src = Ducks.allDucksArray[duck2].src;
   image3.src = Ducks.allDucksArray[duck3].src;
