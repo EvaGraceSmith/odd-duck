15db23460460b53eac4869ffd468046e5c95f139
EvaGraceSmith <smithevagrace@gmail.com>
2022-12-02 23:47:33 -0600

    adding local storage

--- js/duck.js
+++ js/duck.js
@@ -27,19 +27,75 @@ console.log(ducksContainer, resultButton, image1, image2, image3);
 
 
 
-function Ducks(name, src){
+function Ducks(name, src, views, click){
   this.name = name;
   this.src = src;
+  if(views){
+    this.views = views;
+  } else {
   //times shown
-  this.views = 0;
+    this.views = 0;
+  }
+  if(click){
+    this.click = click;
+  } else {
   //times clicked on
-  this.click = 0;
-
+    this.click = 0;
+  }
   //   As we create new instances of our duck objects we can push those into array using the 'this' and the .push()
   // built in array method
   Ducks.allDucksArray.push(this);
 
 }
+
+
+//we need a way to get stuff back from local storage add to our original objects and then render all rounds to vote again.
+let savedDucksString = localStorage.getItem('savedDucks');
+console.log('Duck strings',savedDucksString);
+//getItem from local storage
+
+// send those through our constructor function
+if(savedDucksString){
+  let arrayOfNotDucksObject = JSON.parse(savedDucksString);
+  console.log('objects that dont know they are Ducks?', arrayOfNotDucksObject);
+  //once we have object we are going to run them through our constructor function so that they are Ducks objects.
+
+  for(let i = 0; i < arrayOfNotDucksObject.length; i++){
+    new Ducks(
+      arrayOfNotDucksObject[i].name,
+      arrayOfNotDucksObject[i].src,
+      arrayOfNotDucksObject[i].views,
+      arrayOfNotDucksObject[i].click
+    );
+  }
+  console.log('sssssss',Ducks.allDuckssArray);
+} else {
+//calling the constructor function
+
+  new Ducks('The luggage', 'img/bag.jpg');
+  new Ducks('The banana', 'img/banana.jpg');
+  new Ducks('The bathroom stand', 'img/bathroom.jpg');
+  new Ducks('The boots', 'img/boots.jpg');
+  new Ducks('The breakfast maker', 'img/breakfast.jpg');
+  new Ducks('The bubblegum', 'img/bubblegum.jpg');
+  new Ducks('The chair', 'img/chair.jpg');
+  new Ducks('The cthulhu monster', 'img/cthulhu.jpg');
+  new Ducks('The dog-duck', 'img/dog-duck.jpg');
+  new Ducks('The dragon', 'img/dragon.jpg');
+  new Ducks('The pen', 'img/pen.jpg');
+  new Ducks('The pet-sweep', 'img/pet-sweep.jpg');
+  new Ducks('The Ducks scissors', 'img/scissors.jpg');
+  new Ducks('The shark', 'img/shark.jpg');
+  new Ducks('The sweep', 'img/sweep.png');
+  new Ducks('The tauntaun bag', 'img/tauntaun.jpg');
+  new Ducks('The unicorn', 'img/unicorn.jpg');
+  new Ducks('The watering can', 'img/water-can.jpg');
+  new Ducks('The wine glass', 'img/wine-glass.jpg');
+
+}
+
+
+
 
 function getRandomNumber(){
   return Math.floor(Math.random() * Ducks.allDucksArray.length);
@@ -80,18 +136,18 @@ function renderDucks(){
 
 function afterconfetti()
 {
-  confettiContainer.style.display = "none";
+  confettiContainer.style.display = 'none';
 }
 //function for our render of results to a list
 function renderResults(){
   console.log('In renderResults()');
   let ul = document.querySelector('ul');
   renderChart();
-  window.setTimeout(afterconfetti, 10000);  
+  window.setTimeout(afterconfetti, 10000);
   resultButton.textContent='Results';
-  ducksContainer.style.display = "none";
-  concludeContainer.style.display = "flex";
-  confettiContainer.style.display = "";
+  ducksContainer.style.display = 'none';
+  concludeContainer.style.display = 'flex';
+  confettiContainer.style.display = '';
 }
 
 // Draw a chart with the duck data.
@@ -143,19 +199,30 @@ function renderChart(){
     },
   };
 
+  //   let gradient = ctx.createLinearGradient(0, 0, 0, 450);
+  //   gradient.addColorStop(0, 'rgba(14, 80, 167, 0.5)');
+  //   g\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ient.addColorStop(0.5, 'rgba(14, 80, 167, 0.25)');
+  //   gradient.addColorStop(1, 'rgba(14, 80, 167, 0)');
+
+
+
   const dataDoughnut = {
     labels: duckNames,
     datasets: [
-  
+
       {
         label: 'Popularity',
         data: votePercentage,
-        backgroundColor: ['rgb(255, 211, 81)'],
-        borderColor: ['rgb(14, 80, 167)'],
-        borderWidth: 1,
+        // backgroundColor: gradient,
+        // background: [radial-gradient(rgb(80, 142, 223), rgb(195, 216, 244), rgb(80, 142, 223))],
+        backgroundColor: ['rgb(14, 80, 167)'],
+        borderColor: ['rgb(255, 211, 81)'],
+        borderWidth: 10,
       },
     ],
   };
+
+
 
 
   //configure the graph
@@ -170,15 +237,15 @@ function renderChart(){
       },
     },
   };
-  
+
   // Get a reference to the Canvas element
   let canvasChart =document.getElementById('myChart');
   //Draw the chart
   const myChart = new Chart (canvasChart, config);
 
-let canvasChart2 =document.getElementById('myChart2');
-//Draw the chart
-const myChart2 = new Chart (canvasChart2, configDoughnut);
+  let canvasChart2 =document.getElementById('myChart2');
+  //Draw the chart
+  const myChart2 = new Chart (canvasChart2, configDoughnut);
 }
 //  * Initialize the global variables, set up needed event handlers, and
 //  * perform the initial render.
@@ -189,30 +256,10 @@ const myChart2 = new Chart (canvasChart2, configDoughnut);
 // An instance of an object can be declared by giving it a unique name that can be used in a program.
 // This process is known as instantiation.
 
-new Ducks('The luggage', 'img/bag.jpg');
-new Ducks('The banana', 'img/banana.jpg');
-new Ducks('The bathroom stand', 'img/bathroom.jpg');
-new Ducks('The boots', 'img/boots.jpg');
-new Ducks('The breakfast maker', 'img/breakfast.jpg');
-new Ducks('The bubblegum', 'img/bubblegum.jpg');
-new Ducks('The chair', 'img/chair.jpg');
-new Ducks('The cthulhu monster', 'img/cthulhu.jpg');
-new Ducks('The dog-duck', 'img/dog-duck.jpg');
-new Ducks('The dragon', 'img/dragon.jpg');
-new Ducks('The pen', 'img/pen.jpg');
-new Ducks('The pet-sweep', 'img/pet-sweep.jpg');
-new Ducks('The pizza scissors', 'img/scissors.jpg');
-new Ducks('The shark', 'img/shark.jpg');
-new Ducks('The sweep', 'img/sweep.png');
-new Ducks('The tauntaun bag', 'img/tauntaun.jpg');
-new Ducks('The unicorn', 'img/unicorn.jpg');
-new Ducks('The watering can', 'img/water-can.jpg');
-new Ducks('The wine glass', 'img/wine-glass.jpg');
-
 //add our event listener to run our handleClick()
 preludeContainer.addEventListener('click', handlePreludeClick);
 ducksContainer.addEventListener('click', handleDucksClick);
-confettiContainer.style.display = "none";
+confettiContainer.style.display = 'none';
 
 function handleDucksIconClick(event)
 {
@@ -265,10 +312,11 @@ function handleDucksClick(event){
     resultButton.addEventListener('click', renderResults);
     // ducksContainer.className = 'no-voting';
     resultButton.textContent='View Results';
-
+    localStorage.setItem('savedDucks', JSON.stringify(Ducks.allDucksArray));
 
   } else {
     renderDucks();
   }
 }
 
+// window.localStorage.setItem(key,JSON.stringify(car));
